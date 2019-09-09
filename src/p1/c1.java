13a6b19a7637cbf45f06ca3ae63f3e74d82c95ca
rohanosp <rohan@osplabs.com>
2019-09-09 16:46:54 +0530

    This is Git 1

---
+++ src/p1/c1.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class c1 {
+	public void Twitter() 
+	{
+	System.out.println("Twitter");
+
+	}
+	public void FaceBook() 
+	{
+	System.out.println("Facebook");
+
+	}
+	public void Instagram() 
+	{
+	System.out.println("Instagram");
+
+	}
+}
